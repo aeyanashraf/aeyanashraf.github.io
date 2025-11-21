d689a7d154a98b8be7e60c0bf2d3bd757a3aa609
aeyanashraf <aeyanashraf@gmail.com>
2025-11-21 01:27:14 -0600

    fix crash

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -25,7 +25,12 @@ module ExternalPosts
     def fetch_from_rss(site, src)
       xml = HTTParty.get(src['rss_url']).body
       return if xml.nil?
-      feed = Feedjira.parse(xml)
+      begin
+        feed = Feedjira.parse(xml)
+      rescue Feedjira::NoParserAvailable, StandardError => e
+        Jekyll.logger.warn "ExternalPosts:", "Skipping #{src['rss_url']} (#{e.class} - #{e.message})"
+        return
+      end
       process_entries(site, src, feed.entries)
     end
 
